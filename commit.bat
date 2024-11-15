45b60cc68452a5bd3c697804b687d25d8f635b8b
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2024-11-15 19:03:48 +0700

    add: lunaticdb libs to main repo

--- commit.bat
+++ commit.bat
@@ -2,11 +2,21 @@ cd translation
 git add .
 git commit
 git push
+cd ..
 
-cd lunatic_link
+cd libs
+
+cd lunalink
 git add .
 git commit
 git push
+cd ..
+
+cd lunaticdb
+git add .
+git commit
+git push
+cd ..
 
 cd ..
 git add .
