26a89840388c1d94bcd6f0ff6058d1acdec4973b
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2024-12-01 21:11:17 +0700

    add: some slight improve for page structures

--- src/structures/page.lua
+++ src/structures/page.lua
@@ -3,17 +3,31 @@ local discordia = require('discordia')
 
 local default = {
   client = nil,
-  pages = nil
-  timeout = 120000
-  language = nil
+  pages = nil,
+  timeout = 120000,
+  language = nil,
+  interaction = nil,
+  message  = nil
 }
 
 function page:init(options)
   options = options or default
   self.client = assert(options.client, 'Client not found')
-  self.pages = assert(options.pages, 'Pages not found')
+  self.pages = assert(type(options.pages) == 'table', 'Pages not found')
   self.language = assert(options.language, 'Language not found')
   self.timeout = options.timeout
+  self.interaction = options.interaction
+  self.message = options.message
+end
+
+function page:run()
+  if not self.interaction and not self.message then
+    error("Missing both message and interaction, please check again!")
+  elseif self.interaction then
+    self:slash(self.interaction)
+  elseif self.message then
+    self:message(self.message)
+  end
 end
 
 function page:slash(interaction)
@@ -21,19 +35,7 @@ function page:slash(interaction)
   if not this.pages then error('Pages are not given.') end
   if #this.page == 0 then return end
 
-  local back_button = discordia.Button({
-    id = "back",
-    emoji = self.client._icons.GLOBAL.arrow_previous,
-    style = "secondary"
-  })
-
-  local next_button = discordia.Button({
-    id = "next",
-    emoji = self.client._icons.GLOBAL.arrow_next,
-    style = "secondary"
-  })
-
-	local row = discordia.Components({ back_button, next_button })
+	local row = page:generate_button_array()
 
 	local page = 1
 	local init_page = this.pages[page]
@@ -48,16 +50,27 @@ function page:slash(interaction)
 
 	local collector = interaction:createCollector('button')
 
-	collector:on('collect', function (interaction)
-	  
-	end)
-	
-  
-  
+	collector:on('collect', function (interaction) end)
 end
 
 function page:message(message)
   
+end
+
+function page:generate_button_array()
+  local back_button = discordia.Button({
+    id = "back",
+    emoji = self.client._icons.GLOBAL.arrow_previous,
+    style = "secondary"
+  })
+
+  local next_button = discordia.Button({
+    id = "next",
+    emoji = self.client._icons.GLOBAL.arrow_next,
+    style = "secondary"
+  })
+
+  return discordia.Components({ back_button, next_button })
 end
 
 return page
