a5e77a5d2c1d15b71ee67a5a989482a6645266da
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2024-12-06 21:29:55 +0700

    add: user_list command improve: prefix pattern ggenerator fix: guild list doesn't show guild id

--- src/commands/premium/user_list.lua
+++ src/commands/premium/user_list.lua
@@ -5,12 +5,12 @@ local command = require('class'):create()
 local page_framework = require('../../structures/page')
 
 function command:init()
-	self.name = { 'pm', 'guild', 'list' }
-	self.description = 'View all existing premium guild!'
+	self.name = { 'pm', 'list' }
+	self.description = 'View all existing premium user!'
 	self.category = 'premium'
 	self.accessableby = { accessableby.admin }
 	self.usage = ''
-	self.aliases = { 'pmgl' }
+	self.aliases = { 'pml' }
 	self.lavalink = false
 	self.playerCheck = false
 	self.usingInteraction = true
@@ -41,28 +41,28 @@ function command:run(client, handler)
     })
 	end
 
-	local guilds = table.map(client._db.preGuild:all(), function (element)
+	local users = table.map(client._db.premium:all(), function (element)
 	  return element.data
 	end)
 
-	local page_num = math.ceil(#guilds / 10)
+	local page_num = math.ceil(#users / 10)
 	if page_num == 0 then page_num = 1 end
 
-	local guild_strings = {}
-	for i = 1, #guilds, 1 do
-	  local guild = guilds[i]
-	  local string_ele = string.format('`%s. %s/%s - %s`', i, guild.redeemedBy.name, guild.id, guild.plan)
-	  table.insert(guild_strings, string_ele)
+	local user_strings = {}
+	for i = 1, #users, 1 do
+	  local user = users[i]
+	  local string_ele = string.format('`%s. %s/%s - %s`', i, user.redeemedBy.username, user.id, user.plan)
+	  table.insert(user_strings, string_ele)
 	end
 
 	local pages = {}
 
   for i = 1, page_num do
-    local str = table.concat(table.slice(guild_strings, i * 10, i * 10 + 10), "\n")
+    local str = table.concat(table.slice(user_strings, i * 10, i * 10 + 10), "\n")
 
     local embed = {
       author = {
-        name = client._i18n:get(handler.language, "command.premium", "guild_list_title")
+        name = client._i18n:get(handler.language, "command.premium", "list_title")
       },
       color = discordia.Color.fromHex(client._config.bot.EMBED_COLOR).value,
       description = str == "" and "  Nothing" or "\n" + str,
@@ -84,7 +84,7 @@ end
 function command:send_specific_page(client, handler, pages, page_num, value)
   if value > page_num then
     local embed = {
-      description = client._i18n:get(handler.language, 'command.premium', 'guild_list_page_notfound', page_num),
+      description = client._i18n:get(handler.language, 'command.premium', 'list_page_notfound', page_num),
       color = discordia.Color.fromHex(client._config.bot.EMBED_COLOR).value,
     }
     return handler:edit_reply({
