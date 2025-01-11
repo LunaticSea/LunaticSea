54e903bffbf071e376ec82124718ab7025b3fb5f
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2025-01-11 12:17:54 +0700

    add: functional for loop, pause, previous, skip, stop, volup, voldown button

--- src/buttons/previous.lua
+++ src/buttons/previous.lua
@@ -7,6 +7,23 @@ function get:name()
 end
 
 function bc:run(client, button, language, player, nplaying, collector)
+  if not player and collector then collector:stop() end
+
+  local previousIndex = player.queue.previous.length
+
+  if #player.queue.previous == 0 or previousIndex == 0 then
+    return reply_interaction(client, button, client.i18n:get(language, 'button.music', 'previous_notfound'))
+  end
+
+  player:previous()
+
+  player.data:set('endMode', 'previous')
+
+  return reply_interaction(
+    client,
+    button,
+    client.i18n:get(language, 'button.music', 'previous_msg')
+  )
 end
 
 return bc
--- src/buttons/volup.lua
+++ src/buttons/volup.lua
@@ -7,6 +7,23 @@ function get:name()
 end
 
 function bc:run(client, button, language, player, nplaying, collector)
+  if not player and collector then collector:stop() end
+
+  local reply_msg = client.i18n:get(language, 'button.music', 'volup_msg', {
+    player.volume + 10,
+  })
+
+  if (player.volume >= 100) then
+    return reply_interaction(
+      client,
+      button,
+      client.i18n.get(language, 'button.music', 'volume_max')
+    )
+  end
+
+  player:setVolume(player.volume + 10)
+
+  return reply_interaction(client, button, reply_msg)
 end
 
 return bc
